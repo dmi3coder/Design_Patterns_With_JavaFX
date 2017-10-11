296f1c0420aa11ee93757d590b19c8d26d1b095f
dmi3coder <dmi3coder@gmail.com>
2017-10-11 18:16:25 +0300

    fixed linux os identification

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -13,19 +12,19 @@ public class Main extends Application {
     static AbstractButtonFactory factory;
 
     static {
-        if (System.getProperty("os.name").contains("linux")) {
+        if (System.getProperty("os.name").toLowerCase().contains("linux")) {
             factory = new LinuxButtonFactory();
         } else factory = new IosButtonFactory();
     }
 
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         GridPane main = (GridPane) root.lookup("#main");
-        main.add(factory.getOSButton("test"),0,0);
+        main.add(factory.getOSButton("test"), 0, 0);
         primaryStage.show();
     }
 
